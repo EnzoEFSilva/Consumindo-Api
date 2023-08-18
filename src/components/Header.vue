75e73c842a66ec2e5de7f4ae17a5165e15603bce
Enzo Furtuoso <enzo.furtoso@youxgroup.com.br>
2023-08-18 16:24:50 -0400

    finalizando header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -53,14 +53,13 @@ export default {
         background-color: var(--color-background-nav);
         width: 100%;
         height: 60px;
-        /* display: flex;
-        justify-content: center;
-        align-items: center; */
     }
 
    nav {
     display: flex;
     justify-content: space-between;
+    height: 60px;
+    align-items: center;
    }
 
     #logo{
@@ -114,6 +113,32 @@ export default {
 
     ul li a {
         color: var(--color-text-light);
+    }
+
+    @media (min-width: 700px) {
+        #menu-button,
+        #menu-logo,
+        #menu-overlay {
+            display: none;
+        }
+
+        #menu-items {
+            display: flex;
+            position: static;
+            width: auto;
+            height: 60px;
+        }
+
+        ul {
+            display: flex;
+            height: 60px;
+            align-items: center;
+        }
+
+        ul li {
+            margin: 0;
+            margin-left: 10px;
+        }
     }
 
 </style>
